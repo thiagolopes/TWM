68e0a985a01ca8f9b2dd79347eaf1288f64f35fb
thiagolopes <thiagolopes@protonmail.com>
2021-10-28 15:20:38 -0300

    Add new grab event and remove tests

--- twm.c
+++ twm.c
@@ -5,14 +5,18 @@
 #include <xcb/xcb.h>
 #include <xcb/xproto.h>
 
+#define XCB_EVENT_BIT_MASK ~0x80
+
+
 int main (int argc, char **argv){
-  printf("twm starts\n");
+  printf("hello, twm starts\n");
 
   /* basic xcb vars */
   xcb_connection_t *con;
   xcb_screen_iterator_t screen;
   xcb_window_t window;
   unsigned short width_in_pixels, height_in_pixels;
+  xcb_generic_event_t *ev;
 
   con = xcb_connect(NULL, NULL);
   if (xcb_connection_has_error(con)){
@@ -26,51 +30,23 @@ int main (int argc, char **argv){
 
   /* atoms from mcwm */
 
-  xcb_grab_key(con, 1, window, XCB_MOD_MASK_2, XCB_NO_SYMBOL,
+  xcb_grab_key(con, 1, window, XCB_MOD_MASK_ANY, XCB_GRAB_ANY,
 	       XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
 
-  xcb_grab_button(con, 0, window, XCB_EVENT_MASK_BUTTON_PRESS |
-		  XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
-		  XCB_GRAB_MODE_ASYNC, window, XCB_NONE, 1, XCB_MOD_MASK_1);
-
-  xcb_grab_button(con, 0, window, XCB_EVENT_MASK_BUTTON_PRESS |
-		  XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
-		  XCB_GRAB_MODE_ASYNC, window, XCB_NONE, 3, XCB_MOD_MASK_1);
+  /* xcb_grab_button(con, 0, window, XCB_EVENT_MASK_BUTTON_PRESS | */
+  /* 		  XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC, */
+  /* 		  XCB_GRAB_MODE_ASYNC, window, XCB_NONE, 1, XCB_MOD_MASK_1); */
 
   xcb_flush(con);
-  xcb_generic_event_t *ev;
-  for (;;)
-    {
-      ev = xcb_wait_for_event(con);
-      switch (ev->response_type & ~0x80) {
-      case XCB_BUTTON_PRESS:
-	printf("AAAAAAAAAAA");
-      case XCB_MOTION_NOTIFY:
-	printf("AAAAAAAAAAA");
-      case XCB_BUTTON_RELEASE:
-	printf("AAAAAAAAAAA");
-      }}
 
-  while(1) {
-    xcb_generic_event_t *event = xcb_wait_for_event(con);
-
-    switch(event->response_type & ~0x80){
+  while((ev = xcb_wait_for_event(con))) {
+    switch (ev->response_type & XCB_EVENT_BIT_MASK) {
     case XCB_KEY_PRESS: {
-      printf("key pressed!!!\n");
-      break;
-    }
-    case XCB_BUTTON_PRESS: {
-      xcb_button_press_event_t *press = (xcb_button_press_event_t *)event;
-      printf("button pressed!!!\n");
-      exit(1);
-    }
-      free(event);
+      printf("Key key was pressed, but don't know which one\n");
     }}
+  }
 
-  char *xterm_args[] = {"xterm", NULL};
-	execvp("xterm", xterm_args);
-
-	/* end wm, disconect server */
-	xcb_disconnect(con);
-	return 0;
+  /* end wm, disconect server */
+  xcb_disconnect(con);
+  return 0;
 }
